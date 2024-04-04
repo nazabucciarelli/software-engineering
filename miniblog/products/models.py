21a40aae311631d430349fc0e1a648b11640f899
Nazareno Bucciarelli <bnazareno03@gmail.com>
2024-04-03 22:03:14 -0300

    add category fk in product entity and learn about look ups

--- miniblog/products/models.py
+++ miniblog/products/models.py
@@ -1,8 +1,17 @@
 from django.db import models
-
+from categories.models import Category
 # Create your models here.
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    category = models.ForeignKey(
+        Category,
+        on_delete = models.SET_NULL,
+        null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.name
