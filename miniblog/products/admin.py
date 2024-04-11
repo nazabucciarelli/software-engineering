92f8e446837896224b9aebcbde6b40c270591cbd
Nazareno Bucciarelli <bnazareno03@gmail.com>
2024-04-10 22:53:47 -0300

    10-04

--- miniblog/products/admin.py
+++ miniblog/products/admin.py
@@ -6,4 +6,10 @@ from products.models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name','price','description')
+    ordering = ('-name','-price') # para que se ordenen por defecto las listas
+    search_fields = ('name','category__name') # Es para que cuando buscamos en el search bar, busquemos por estos dos parametros
+    empty_value_display = "Aun no tiene valor"
+    list_filter = ("category",)
+    list_editable = ('price',) # Convierte el campo en editable
+    
+    list_display = ('name','price','description','category')
